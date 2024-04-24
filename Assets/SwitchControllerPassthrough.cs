75ac56aa7633bd4b084201ee89886204a3653204
Dimitri VINET (on Sarah's PC <dimitri.user@dimitrivinet.com>
2024-04-24 14:52:17 +0200

    add switch controller passthrough

---
+++ Assets/SwitchControllerPassthrough.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Joycon
+{
+    public string Id;
+    public string Name;
+    public string Guid;
+    public float PowerLevel;
+    public float[] Axes;
+    public float[] Buttons;
+    public float[] Hats;
+}
+
+public class SwitchControllerPassthrough : MonoBehaviour
+{
+    [SerializeField]
+    private RedisManager Redis;
+    public Dictionary<string, Joycon> Joycons;
+
+    public float ParseWithDefault(string toParse, float defaultValue)
+    {
+        float ret = defaultValue;
+        try
+        {
+            ret = float.Parse(toParse, null);
+        }
+        catch
+        {}
+
+        return ret;
+    }
+
+    public int ParseWithDefault(string toParse, int defaultValue)
+    {
+        int ret = defaultValue;
+        try
+        {
+            ret = int.Parse(toParse, null);
+        }
+        catch
+        {}
+
+        return ret;
+    }
+
+
+    void Awake()
+    {
+        Joycons = new();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string connectedJoyconIds = Redis.Get("joycon_ids");
+        if (connectedJoyconIds == null)  // error with redis, or passthrough program not up
+            return;
+
+        string[] connectedJoyconIdsList = connectedJoyconIds.Split(':');
+
+        // update connected joycons values, creating when necessary
+        for (int i = 0; i < connectedJoyconIdsList.Length; i++)
+        {
+            string k = connectedJoyconIdsList[i];
+            if (!Joycons.ContainsKey(k))
+            {
+                Joycons.Add(k, new Joycon());
+                Joycons[k].Name = Redis.Get($"{k}:name");
+                Joycons[k].Guid = Redis.Get($"{k}:guid");
+
+                int defaultNum = 0;
+                string numAxesStr = Redis.Get($"{k}:num_axes");
+                int numAxes = ParseWithDefault(numAxesStr, defaultNum);
+                Joycons[k].Axes = new float[numAxes];
+
+                string numButtonsStr = Redis.Get($"{k}:num_buttons");
+                int numButtons = ParseWithDefault(numAxesStr, defaultNum);
+                Joycons[k].Buttons = new float[numButtons];
+
+                string numHatsStr = Redis.Get($"{k}:num_hats");
+                int numHats = ParseWithDefault(numHatsStr, defaultNum);
+                Joycons[k].Hats = new float[numHats];
+            }
+
+            string tempStr;
+            float temp;
+            float tempDef = 0.0f;
+
+            tempStr = Redis.Get($"{k}:power_level");
+            temp = ParseWithDefault(tempStr, tempDef);
+            Joycons[k].PowerLevel = temp;
+            
+            for (int j = 0; j < Joycons[k].Axes.Length; j++)
+            {
+                tempStr = Redis.Get($"{k}:axes:{j}");
+                temp = ParseWithDefault(tempStr, tempDef);
+                Joycons[k].Axes[j] = temp;
+            }
+            for (int j = 0; j < Joycons[k].Buttons.Length; j++)
+            {
+                tempStr = Redis.Get($"{k}:buttons:{j}");
+                temp = ParseWithDefault(tempStr, tempDef);
+                Joycons[k].Buttons[j] = temp;
+            }
+            for (int j = 0; j < Joycons[k].Hats.Length; j++)
+            {
+                tempStr = Redis.Get($"{k}:hats:{j}");
+                temp = ParseWithDefault(tempStr, tempDef);
+                Joycons[k].Hats[j] = temp;
+            }
+        }
+
+        // cleanup disconnected joycons
+        List<string> keysToRemove = new();
+        foreach (var k in Joycons.Keys)
+        {
+            if (!Array.Exists(connectedJoyconIdsList, el => el == k))
+            {
+                keysToRemove.Add(k);
+            }
+        }
+        foreach (var kr in keysToRemove)
+        {
+            Joycons.Remove(kr);
+        }
+    }
+}
